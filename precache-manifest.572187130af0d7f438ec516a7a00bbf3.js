abc8297f135497850040a84f98e9d7e9de6a60b5
Admin <admin@MacBook-Pro-Admin-2.local>
2019-05-01 17:17:57 +0300

    Updates

---
+++ precache-manifest.572187130af0d7f438ec516a7a00bbf3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cbbb66fd38bf679eabee0cf42f14e69",
+    "url": "/static/media/refresh.7cbbb66f.png"
+  },
+  {
+    "revision": "663fb77ed1e2c5d13437",
+    "url": "/static/css/main.a83551c0.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "35810d5f3acabe76cf3d9768c3461546",
+    "url": "/static/media/up.35810d5f.png"
+  },
+  {
+    "revision": "ba25545dbd30eb07454e",
+    "url": "/static/js/2.f4dbf2d4.chunk.js"
+  },
+  {
+    "revision": "24c4437a521a2cfc665c944a6f8769dc",
+    "url": "/static/media/source2.24c4437a.gif"
+  },
+  {
+    "revision": "0b3ee92e0b923f1175b273472b86e147",
+    "url": "/static/media/logo.0b3ee92e.png"
+  },
+  {
+    "revision": "1d51e67f174c383dea76b9cd0367844b",
+    "url": "/static/media/logo3.1d51e67f.png"
+  },
+  {
+    "revision": "7b158281627b719ccda49cd58fd100d8",
+    "url": "/static/media/bg1.7b158281.jpg"
+  },
+  {
+    "revision": "663fb77ed1e2c5d13437",
+    "url": "/static/js/main.09559e42.chunk.js"
+  },
+  {
+    "revision": "5b1dff311a31f40babd677aac93b53a3",
+    "url": "/static/media/video-play-icon-transparent-1.5b1dff31.png"
+  },
+  {
+    "revision": "7b20d39d1d00df3b4780f4ffe8a70e5d",
+    "url": "/static/media/thumb1.7b20d39d.png"
+  },
+  {
+    "revision": "8ba3391eb9f58ef83b959400b15a409b",
+    "url": "/static/media/IMDB_Logo_2016.8ba3391e.svg"
+  },
+  {
+    "revision": "286e82049a5036c515405f0da584f5d0",
+    "url": "/static/media/booster_s_shifting_cubeing_s_by_boost_master-d676ife.286e8204.gif"
+  },
+  {
+    "revision": "7458576951f35aab54fa1a1f830da4f3",
+    "url": "/static/media/link.74585769.png"
+  },
+  {
+    "revision": "e537824b0431f1f9a36696e84e64808f",
+    "url": "/static/media/600px-Octicons-mark-github.svg.e537824b.png"
+  },
+  {
+    "revision": "ba25545dbd30eb07454e",
+    "url": "/static/css/2.3f5d369f.chunk.css"
+  },
+  {
+    "revision": "0c738691b4da908198f5413ea3a9503d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.2a14c8e2e8a15e46a0a60a616b0fdc1d.js"
+  "/precache-manifest.572187130af0d7f438ec516a7a00bbf3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
